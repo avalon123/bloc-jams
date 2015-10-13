47b8a0cab78181539201b6acd9f019ff40316c52
avalon123 <bzhu9035@gmail.com>
2015-10-13 13:10:50 -0700

    checkpoint-23 finish

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,7 +1,5 @@
-var animatePoints = function() {
-
-    var points = document.getElementsByClassName('point');
-
+var pointsArray = document.getElementsByClassName('point');
+var animatePoints = function(points) {
     var revealPoint = function() {
         for (var i=0;i<points.length;i++){
         points[i].style.opacity = 1;
@@ -12,4 +10,18 @@ var animatePoints = function() {
     };
     revealPoint();
 };
-//animatePoints();
+ window.onload = function() {
+
+     var sellingPoints = document.getElementsByClassName('selling-points')[0];
+     var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+     if (window.innerHeight > 950) {
+         animatePoints(pointsArray);
+     }
+     window.addEventListener("scroll", function(event) {
+
+         if (document.body.scrollTop >= scrollDistance) {
+             animatePoints(pointsArray);   
+         }
+
+     })
+ };

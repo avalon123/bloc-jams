3bdfff20a0162b0df1fabc6e3c589866b775afcb
avalon123 <bzhu9035@gmail.com>
2015-10-20 20:10:18 -0700

    checkpoint-31 fixed

--- scripts/album.js
+++ scripts/album.js
@@ -41,24 +41,25 @@ var createSongRow = function(songNumber, songName, songLength) {
       ;
  
      var $row = $(template);
-     var clickHandler = function() {
-	var songNumber = $(this).attr('data-song-number');
+    
 
-	if (currentlyPlayingSong !== null) {
-		// Revert to song number for currently playing song because user started playing new song.
-		var currentlyPlayingCell = $('.song-item-number[data-song-number="' + currentlyPlayingSong + '"]');
-		currentlyPlayingCell.html(currentlyPlayingSong);
-	}
-	if (currentlyPlayingSong !== songNumber) {
-		// Switch from Play -> Pause button to indicate new song is playing.
-		$(this).html(pauseButtonTemplate);
-		currentlyPlayingSong = songNumber;
-	} else if (currentlyPlayingSong === songNumber) {
-		// Switch from Pause -> Play button to pause currently playing song.
-		$(this).html(playButtonTemplate);
-		currentlyPlayingSong = null;
-	}
-};
+         var clickHandler = function() {
+         var songNo = $(this).attr('data-song-number');
+             console.log(songNo);
+         if(currentlyPlayingSong !== null){
+         var currenting = $('.song-item-number[data-song-number="' + currentlyPlayingSong + '"]');
+         currenting.html(currentlyPlayingSong);
+         }
+         if(currentlyPlayingSong !== songNo){
+          $(this).html(pauseButtonTemplate);
+          currentlyPlayingSong = songNo;
+         }
+         else if(currentlyPlayingSong === songNo){
+         $(this).html(playButtonTemplate);
+         currentlyPlayingSong = null;
+         }
+     };
+    
      var onHover = function(event) {
         var songNumberCell = $(this).find('.song-item-number');
         var songNumber = songNumberCell.attr('data-song-number');

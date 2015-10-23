fb61ce98965f86acae1ed2069e1cfc5dc2e80c03
avalon123 <bzhu9035@gmail.com>
2015-10-22 18:56:33 -0700

    checkpoint-33

--- scripts/album.js
+++ scripts/album.js
@@ -1,7 +1,21 @@
 var setSong=function(songNumber){
+    if (currentSoundFile) {
+         currentSoundFile.stop();
+     }
     currentlyPlayingSongNumber = songNumber;
     currentSongFromAlbum = currentAlbum.songs[songNumber-1];
-};
+     currentSoundFile = new buzz.sound(currentSongFromAlbum.audioUrl, {
+         formats: [ 'mp3' ],
+         preload: true
+     });
+ setVolume(currentVolume);
+ };
+ 
+ var setVolume = function(volume) {
+     if (currentSoundFile) {
+         currentSoundFile.setVolume(volume);
+     }
+ };
 var getSongNumberCell=function(number){
  return $('.song-item-number[data-song-number="' + number + '"]');
 };
@@ -18,7 +32,7 @@ var createSongRow = function(songNumber, songName, songLength) {
     var $row = $(template);
 
 
- var clickHandler = function() {
+var clickHandler = function() {
 
   	var songNumber = parseInt($(this).attr('data-song-number'));
 
@@ -26,17 +40,29 @@ var createSongRow = function(songNumber, songName, songLength) {
     if (currentlyPlayingSongNumber !== null) {
              var currentlyPlayingCell =getSongNumberCell(currentlyPlayingSongNumber);
              currentlyPlayingCell.html(currentlyPlayingSongNumber);
+             
      }
 
     if (currentlyPlayingSongNumber !== songNumber) {
          $(this).html(pauseButtonTemplate);
          setSong(songNumber);
+         currentSoundFile.play();
+         currentSongFromAlbum = currentAlbum.songs[songNumber - 1];
          updatePlayerBarSong();
+         
     } else if (currentlyPlayingSongNumber === songNumber) {
-         $(this).html(playButtonTemplate);
-         $('.main-controls .play-pause').html(playerBarPlayButton);
-         currentlyPlayingSongNumber = null;
-         currentSongFromAlbum = null;
+         if (currentSoundFile.isPaused()) 
+         {
+               $(this).html(pauseButtonTemplate);
+               $('.main-controls .play-pause').html(playerBarPauseButton);
+               currentSoundFile.play();
+          } 
+        else {
+                $(this).html(playButtonTemplate);
+                $('.main-controls .play-pause').html(playerBarPlayButton);
+                currentSoundFile.pause();   
+            }
+
      }
 
  };
@@ -100,14 +126,9 @@ var nextSong = function() {
     }
     
     // Set a new current song
-    currentlyPlayingSongNumber = currentSongIndex + 1;
-    currentSongFromAlbum = currentAlbum.songs[currentSongIndex];
-
-    // Update the Player Bar information
-    $('.currently-playing .song-name').text(currentSongFromAlbum.name);
-    $('.currently-playing .artist-name').text(currentAlbum.artist);
-    $('.currently-playing .artist-song-mobile').text(currentSongFromAlbum.name + " - " + currentAlbum.name);
-    $('.main-controls .play-pause').html(playerBarPauseButton);
+    setSong(currentSongIndex + 1);
+    currentSoundFile.play();
+    updatePlayerBarSong();
     
     var lastSongNumber = getLastSongNumber(currentSongIndex);
     var $nextSongNumberCell = getSongNumberCell(currentlyPlayingSongNumber );
@@ -120,8 +141,6 @@ var nextSong = function() {
 
 var previousSong = function() {
     
-    // Note the difference between this implementation and the one in
-    // nextSong()
     var getLastSongNumber = function(index) {
         return index == (currentAlbum.songs.length - 1) ? 1 : index + 2;
     };
@@ -134,15 +153,9 @@ var previousSong = function() {
         currentSongIndex = currentAlbum.songs.length - 1;
     }
     
-    // Set a new current song
-    currentlyPlayingSongNumber = currentSongIndex + 1;
-    currentSongFromAlbum = currentAlbum.songs[currentSongIndex];
-
-    // Update the Player Bar information
-    $('.currently-playing .song-name').text(currentSongFromAlbum.name);
-    $('.currently-playing .artist-name').text(currentAlbum.artist);
-    $('.currently-playing .artist-song-mobile').text(currentSongFromAlbum.name + " - " + currentAlbum.name);
-    $('.main-controls .play-pause').html(playerBarPauseButton);
+    setSong(currentSongIndex + 1);
+    currentSoundFile.play();
+    updatePlayerBarSong();
     
     var lastSongNumber = getLastSongNumber(currentSongIndex);
     var $previousSongNumberCell = getSongNumberCell(currentlyPlayingSongNumber);
@@ -167,6 +180,8 @@ var playerBarPauseButton = '<span class="ion-pause"></span>';
 var currentAlbum = null;
 var currentlyPlayingSongNumber = null;
 var currentSongFromAlbum = null;
+var currentSoundFile = null;
+var currentVolume = 80;
 var $previousButton = $('.main-controls .previous');
 var $nextButton = $('.main-controls .next');
  $(document).ready(function(){
--- scripts/fixtures.js
+++ scripts/fixtures.js
@@ -5,11 +5,11 @@
      year: '1881',
      albumArtUrl: 'assets/images/album_covers/01.png',
      songs: [
-         { name: 'Blue', length: '4:26' },
-         { name: 'Green', length: '3:14' },
-         { name: 'Red', length: '5:01' },
-         { name: 'Pink', length: '3:21' },
-         { name: 'Magenta', length: '2:15' }
+        { name: 'Blue', length: '4:26', audioUrl: 'C:/Users/excalibur/bloc/bloc-jams/assets/music/blue' },
+         { name: 'Green', length: '3:14', audioUrl: 'C:/Users/excalibur/bloc/bloc-jams/assets/music/green' },
+         { name: 'Red', length: '5:01', audioUrl: 'C:/Users/excalibur/bloc/bloc-jams/assets/music/red' },
+         { name: 'Pink', length: '3:21', audioUrl: 'C:/Users/excalibur/bloc/bloc-jams/assets/music/pink' },
+         { name: 'Magenta', length: '2:15', audioUrl: 'C:/Users/excalibur/bloc/bloc-jams/assets/music/magenta' } 
      ]
  };
  
@@ -27,3 +27,4 @@
          { name: 'Wrong phone number', length: '2:15' }
      ]
  };
+
